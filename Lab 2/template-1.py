36c2df60922e21af96d198bf550c6a377f21a265
McKinley Humble <127350127+mhumble18@users.noreply.github.com>
2026-03-26 14:33:05 -0500

    added lab 2 files

---
+++ Lab 2/template-1.py
@@ -0,0 +1,10 @@
+# Le Chiffre
+# Team Name:
+# Members:
+# A program that attempts to solve cipher texts that are shifted by some number.
+# For the thresholds, we were able to get ciphertext-1= , ciphertext-2= , ciphertext-3= , ciphertext-4= , and the doubly encrypted=
+
+
+
+# the alphabet
+ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()-_=+[{]}\\|;:'\",<.>/? "

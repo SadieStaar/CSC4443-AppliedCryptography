8f234e7bd6ce2094584b3486e98fb890b1699c8f
SadieAnn <127803877+SadieStaar@users.noreply.github.com>
2026-03-26 21:09:23 -0500

    Added comments

--- Lab 2/Sadie-Lab 2.py
+++ Lab 2/Sadie-Lab 2.py
@@ -4,9 +4,10 @@
 # A program that attempts to solve cipher texts that are shifted by some number.
 # For the thresholds, we were able to get ciphertext-1= , ciphertext-2= , ciphertext-3= , ciphertext-4= , and the doubly encrypted=
 
+# most of this code was ripped from Lab 1, as vignere and caesar are very similar
 import sys
 
-# the alphabet
+# GLOBAL VARIABLES
 ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()-_=+[{]}\\|;:'\",<.>/? "
 ALPHABET_3 = ""
 DICTIONARY = {}
@@ -14,26 +15,28 @@ REVERSE_DICT = {}
 POTENTIAL_KEYS = []
 COMMON_WORDS = []
 
+# TEXT TO STRING: turns a file input into a string
 def txtToString(infile):
     str = ""
     for line in infile:
         str += line
     return str
 
+# SET UP CIPHER DICTIONARY: sets up a dictionary with the characters of the chosen alphabet and their values
+# also sets up a reverse dictionary for easier, less iterative-y translation back
 def setUpCipherDictionary(str):
     for i in range(len(str)):
         DICTIONARY[str[i]] = i
         REVERSE_DICT[i] = str[i]
 
-def setUpKeyDictionary(str):
-    for i in range(len(str)):
-        POTENTIAL_KEYS.append(i)
-
+# SET UP COMMON WORDS: Takes a file and turns its contents into an array
+# used for both the frequency analyzer and the potential vignere keys
 def setUpCommonWords(dictionaryFile):
     commonWordString = txtToString(open(dictionaryFile, 'r'))
     result = commonWordString.split()
     return result
 
+# CIPHER TO NUMBER: converts the characters of a ciphertext to the alphabets' values
 def cipherToNumber(ciphertext):
     result = []
     for i in ciphertext:
@@ -43,6 +46,7 @@ def cipherToNumber(ciphertext):
             result.append(i)
     return result
 
+# VIGENERE: similar to the rotation from Lab 1, but has a dynamically shifting rotation value
 def vignere(ciphertextArray, keyArray):
     rotatedText = ""
     cipherPlace = 0
@@ -51,6 +55,8 @@ def vignere(ciphertextArray, keyArray):
     try:
         while True:
             if isinstance(ciphertextArray[cipherPlace], int):
+                # rotate (add) the key's value, then shift the key over (since that spot is used
+                # also makes sure the key loops back when it's reached the end
                 rotatedIndex = (ciphertextArray[cipherPlace] - keyArray[keyPlace] + dictlen) % dictlen
                 rotatedText += REVERSE_DICT[rotatedIndex]
                 keyPlace = (keyPlace + 1) % len(keyArray)
@@ -61,6 +67,7 @@ def vignere(ciphertextArray, keyArray):
     except IndexError:
         return rotatedText
 
+# FREQUENCY ANALYZER: checks if the deciphered text could be plaintext by comparing it to some known words
 def frequencyAnalyzer(decrypted):
     testing = decrypted.lower().split(' ')
     score = 0
@@ -69,6 +76,7 @@ def frequencyAnalyzer(decrypted):
     if score > (len(testing) / 50): return True
     else: return False
 
+# MAIN
 if __name__ == '__main__':
     # setup text to be decoded
     setUpCipherDictionary(ALPHABET)

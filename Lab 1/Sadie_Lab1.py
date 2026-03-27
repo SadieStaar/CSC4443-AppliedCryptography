3e680edc5c199ea3756bdec252f8738600d8e9d6
SadieAnn <127803877+SadieStaar@users.noreply.github.com>
2026-03-26 20:57:32 -0500

    Added comments

--- Lab 1/Sadie_Lab1.py
+++ Lab 1/Sadie_Lab1.py
@@ -6,29 +6,34 @@
 
 import sys
 
-# the alphabet
+# Global variables setup
 ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()-_=+[{]}\\|;:'\",<.>/? "
 ALPHABET_3 = " -,;:!?/.'\"()[]$&#%012345789aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxyYzZ" # used on ciphertext3
 DICTIONARY = {}
 REVERSE_DICT = {}
 COMMON_WORDS = []
 
+# TEXT TO STRING: Takes in a text file and converts the whole file into one long string for processing
 def txtToString(infile):
     str = ""
     for line in infile:
         str += line
     return str
 
+# SET UP CIPHER DICTIONARY: Sets up a value-to-character dictionary for string conversion later.
+# also sets up a reverse dictionary to save on iterations for future reconversion
 def setUpCipherDictionary(str):
     for i in range(len(str)):
         DICTIONARY[str[i]] = i
         REVERSE_DICT[i] = str[i]
 
+# SET UP COMMON WORDS: takes a file of commonly used english words and converts it into an array; used for frequency testing
 def setUpCommonWords(dictionaryFile):
     commonWordString = txtToString(open(dictionaryFile, 'r'))
     result = commonWordString.split()
     return result
 
+# CIPHER TO NUMBER: converts a string of ciphertext into an array of it's value equivalent using the dictionaries from earlier
 def cipherToNumber(ciphertext):
     result = []
     for i in ciphertext:
@@ -38,6 +43,8 @@ def cipherToNumber(ciphertext):
             result.append(i)
     return result
 
+# ROTATE: arithmetic function that 'rotates' each character value by a certain rotation value
+# accounts for both overflow and underflow values
 def rotate(numArray, rotation):
     dictLen = len(DICTIONARY)
     rotatedText = ""
@@ -49,6 +56,8 @@ def rotate(numArray, rotation):
             rotatedText += numArray[i]
     return rotatedText
 
+# FREQUENCY ANALYZER: turns the decrypted string into an array, and compares its elements with common words
+# then, a score is generated to decide if the deciphered text is the potential plaintext
 def frequencyAnalyzer(decrypted):
     testing = decrypted.lower().split(' ')
     score = 0
@@ -57,6 +66,7 @@ def frequencyAnalyzer(decrypted):
     if score > (len(testing) / 50): return True
     else: return False
 
+# MAIN FUNCTION
 if __name__ == '__main__':
     # setup text to be decoded
     setUpCipherDictionary(ALPHABET)
@@ -67,8 +77,12 @@ if __name__ == '__main__':
     ConvertedCiphertext = cipherToNumber(Ciphertext)
     rotations = 0
     while True:
+        # iterate the rotation, then rerotate the original values
         rotations = rotations + 1
         workingText = rotate(ConvertedCiphertext, rotations)
+        
+        # check if what was processed looks like plain english
         if (frequencyAnalyzer(workingText)):
             break
+    
     print("Shift of " + str(rotations) + ":\n" + workingText )
